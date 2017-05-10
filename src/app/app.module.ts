ada109d090a5d1fa1a6dc6a13315d8664e50b65b
Vlad Utkin <viad.2000@yandex.ru>
2017-05-11 00:09:50 +0300

    Начало работы над интерфейсом.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MdButtonModule, MdCheckboxModule, MdToolbarModule, MdSidenavModule, MdCardModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { MdInputModule } from "@angular/material";
 
 @NgModule({
   declarations: [
@@ -12,7 +15,14 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    BrowserAnimationsModule,
+    HttpModule,
+    MdButtonModule,
+    MdCheckboxModule,
+    MdToolbarModule,
+    MdSidenavModule,
+    MdCardModule,
+    MdInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
